7473b9906b3e65000511c49adcabbbd112f8f17a
laurence <massrubydev@gmail.com>
2025-06-09 15:25:40 -0400

    add libs needed for playwright

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,16 @@
 # Exit on error
 set -o errexit
 
+apt-get update && apt-get install -y \
+  libgtk-4-1 \
+  libgraphene-1.0-0 \
+  libgstreamer-gl1.0-0 \
+  gstreamer1.0-plugins-bad \
+  libenchant-2-2 \
+  libsecret-1-0 \
+  libmanette-0.2-0 \
+  libgles2
+  
 bundle install
 playwright install
 bin/rails assets:precompile
